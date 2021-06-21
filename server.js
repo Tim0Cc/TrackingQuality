0ff019f9bdf7011838abe4992d5a2f26d0dd005f
Tim Cecatka <tim.cecatka@gmail.com>
2021-06-21 18:39:53 +0200

    server.js setup

---
+++ server.js
@@ -0,0 +1,52 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const expressLayout = require('express-ejs-layouts')
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('passport')
+
+require('./config/passport')(passport)
+
+const usersRouter = require('./routes/users')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.on('error', error => console.error(error))
+mongoose.connection.once('open', () => console.log('Database connection established'))
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+
+app.use(expressLayout)
+app.use(methodOverride)
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  next()
+})
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  res.locals.error = req.flash('error')
+  next()
+})
+
+app.use('/login', usersRouter)
+
+app.listen(process.env.PORT || 3000)
