9a4c16f62c917de608a5129c8e65626a76613483
Tim Cecatka <tim.cecatka@gmail.com>
2021-07-11 18:09:26 +0200

    Artist READ

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const Artist = require('../models/artist')
 // const passport = require('passport')
 
 // const { ensureAuthenticated } = require('../config/auth')
 
-router.get('/', (req, res) =>{
-  res.render('index')
+router.get('/', async (req, res) =>{
+  try {
+    const artists = await Artist.find({})
+    res.render('index', { artists })
+  } catch (error) {
+    console.error(error)
+    res.redirect('/error')
+  }
+})
+
+router.get('/error', async (req, res) =>{
+  res.render('error')
 })
 
 // router.get('/test', ensureAuthenticated, (req, res) =>{
