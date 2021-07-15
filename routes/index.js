f04481f1cc3989cb2b9fcc15c2b40a8bb2b9ed02
Tim Cecatka <tim.cecatka@gmail.com>
2021-07-15 17:16:52 +0200

    artists.ejs & publications.ejs & institutions.ejs

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,33 @@ router.get('/', async (req, res) =>{
   }
 })
 
+router.get('/artists', async (req, res) =>{
+  try {
+    res.render('../public/pages/artists', { mailadress: process.env.MAILADRESS })
+  } catch (error) {
+    console.error(error)
+    res.redirect('/')
+  }
+})
+
+router.get('/publications', async (req, res) =>{
+  try {
+    res.render('../public/pages/publications', { mailadress: process.env.MAILADRESS })
+  } catch (error) {
+    console.error(error)
+    res.redirect('/')
+  }
+})
+
+router.get('/institutions', async (req, res) =>{
+  try {
+    res.render('../public/pages/institutions', { mailadress: process.env.MAILADRESS })
+  } catch (error) {
+    console.error(error)
+    res.redirect('/')
+  }
+})
+
 router.get('/impressum', (req, res) =>{
   res.render('../public/pages/impressum', { mailadress: process.env.MAILADRESS })
 })
