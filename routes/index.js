67324dc5a2177a77eaac98a702df6eee04c1a151
Tim Cecatka <tim.cecatka@gmail.com>
2021-07-16 00:10:11 +0200

    artists & links

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.get('/artists', async (req, res) =>{
     const artists = await Artist.find({}).sort('name').exec()
     const publications =  await Publication.find({}).sort('name').populate('artists').exec()
     const institutions =  await Institution.find({}).sort('name').populate('artists').exec()
-    const links =  await Institution.find({}).sort('name').populate('artists').populate('publications').populate('institutions').exec()
+    const links =  await Link.find({}).sort('type').populate('artists').populate('publications').populate('institutions').exec()
     res.render('../public/pages/artists', { artists, publications, institutions, links, mailadress: process.env.MAILADRESS })
   } catch (error) {
     console.error(error)
